89426b8802565e3b05c30c7f9466fcc99a59054c
zkharit <zkharit@gmail.com>
2024-01-11 00:29:40 -0500

    create Wallet struct/impl block

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use k256::{SecretKey, ecdsa::{SigningKey, VerifyingKey, Signature, signature::Signer, signature::Verifier}};
+use k256::{SecretKey, ecdsa::{SigningKey, VerifyingKey, Signature, signature::Signer}, PublicKey, Secp256k1};
 use rand_core::OsRng;
 use ripemd::Ripemd160;
 use sha2::{Sha256, Digest};
 
 fn main() {
+    // Test Vectors:
     // Test secret key found on: https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses#How_to_create_Bitcoin_Address
-    // Import key, as well as used for static key testing
     // let secret_key_material: &[u8] = &[24, 225, 74, 123, 106, 48, 127, 66, 106, 148, 248, 17, 71, 1, 231, 200, 231, 116, 231, 249, 164, 126, 44, 32, 53, 219, 41, 162, 6, 50, 23, 37];
+
+    // Test secret key found on: https://en.bitcoin.it/wiki/Wallet_import_format
+    // let secret_key_material: &[u8] = &[12, 40, 252, 163, 134, 199, 162, 39, 96, 11, 47, 229, 11, 124, 174, 17, 236, 134, 211, 191, 31, 190, 71, 27, 232, 152, 39, 225, 157, 114, 170, 29];
+
     // let secret_key_material: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
 
     // This is 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1 which I believe is supposed to be the max number in ecdsa, but this breaks when attempting to generate a key from it (or minus 1)
@@ -18,118 +22,162 @@ fn main() {
     
     // let secret_key = SecretKey::from_bytes(secret_key_material.into()).unwrap();
 
-    // generate private key
-    let secret_key = SecretKey::random(&mut OsRng);
-    // add the .to_vec here so we can manipulate it later (for private key storage/encoding to wallet import format (WIF))
-    let secret_key_bytes = secret_key.to_bytes().to_vec(); // remove .to_vec for SigningKey::from_bytes(), need to think about whats the best option here
-    println!("Private Key:");
-    println!("{:X?}", secret_key_bytes);
-    println!("");
+    let mut block_wallet: Wallet = Wallet::new();
 
-    // generate signing key from the secret key (same key, different type)
-    let signing_key = SigningKey::from(&secret_key);
-    // let signing_key = SigningKey::from_bytes(&secret_key_bytes).unwrap(); // another option of restoring the priv/signing key 
-    let signing_key_bytes = signing_key.to_bytes();
-    println!("Signing Key:");
-    println!("{:X?}", signing_key_bytes);
-    println!("");
+    println!("{:X?}", block_wallet.address());
+    println!("{:X?}", block_wallet.wif_private_key());
+}
 
-    // test message to sign
-    let message: Vec<u8> = vec![0x03, 0xED, 0x73, 0x45, 0xC0];
-    println!("Message to Sign:");
-    println!("{:X?}", message);
-    println!("");
+pub struct Wallet {
+    private_key: elliptic_curve::SecretKey<Secp256k1>,
+    public_key: elliptic_curve::PublicKey<Secp256k1>,
+    address: String,
+    signing_key: k256::ecdsa::SigningKey,
+    verifying_key: k256::ecdsa::VerifyingKey,
+    wif_private_key: String,
+}
 
-    // sign the message
-    let signature: Signature = signing_key.sign(&message);
-    println!("Signed Message:");
-    println!("{:?}", signature);
-    println!("");
-
-    // generate public key from private key
-    let public_key = secret_key.public_key();
-    // convert to [u8] array in comprsesed format with 0x02 or 0x03 prefix (if y is even/odd) 
-    let public_key_sec_bytes = public_key.to_sec1_bytes();
-    println!("Compressed Public Key:");
-    println!("{:X?}", public_key_sec_bytes);
-    println!("");
-
-    // generate verifying key from the public key (~same key (minus the odd/even bit and I think it needs the entire Y value), but the same content in general)
-    let verifying_key = VerifyingKey::from(&public_key);
-    let verifying_key_bytes = verifying_key.to_sec1_bytes();
-    println!("Verifying Key:");
-    println!("{:X?}", verifying_key_bytes);
-    println!("");
-
-    // assert that the signature matches the public key
-    assert!(verifying_key.verify(&message, &signature).is_ok());
-    println!("VerifyingKey verified Signed Message from SigningKey");
-    println!("");
-
-    // sha256 the compressed private key
-    let mut sha256_hasher: Sha256 = Sha256::new();
-    sha256_hasher.update(public_key_sec_bytes);
-    let sha256_compressed_pub_key = sha256_hasher.finalize();
-    println!("Sha256 Compressed Public Key:");
-    println!("{:X?}", sha256_compressed_pub_key);
-    println!("");
-
-    // ripemd160  the sha(compressed pub key)
-    let mut ripemd160_hasher = Ripemd160::new();
-    ripemd160_hasher.update(sha256_compressed_pub_key);
-    let ripemd_sha256_pub_key = ripemd160_hasher.finalize();
-    println!("RIPEMD-160 Sha256 Compressed Public Key:");
-    println!("{:X?}", ripemd_sha256_pub_key);
-    println!("");
-
-    // insert version bytes to front of ripe160(sha(pub key))
-    let mut vec_of_ripe_sha_pub_key = ripemd_sha256_pub_key.to_vec();
-    // version bytes to preface addresses with "BLoCK"
-    // also adds a 1 at the end of BLoCK, which isn't necessarily great, but can be used as a kind of visual verseion number, if later addresses are generated
-    // the last character is not guarnteed though, theoretically a large enough number could make this wrap to a 2 (or maybe not, infeasible and not really needed to test)
+impl Wallet {
+    // version bytes for prefecing addresses with BLoCK
+    // also adds a 1 at the end of BLoCK, which isn't necessarily great, but can be used as a kind of visual version number, if later addresses types are generated
+    // the last character (1) I dont think is guarnteed though, theoretically a large enough number could make this wrap to a 2 (or maybe not, infeasible to test and not super valuable information at this time)
     // security of address is not sacrified due to additional size i.e. not subtracting from original address to add in prefix address = prefix (BLoCK1) + normal address size = normal address size + 6 (BLoCK1) 39bits total
-    let version_bytes: Vec<u8> = vec![0x03, 0xED, 0x73, 0x45, 0xC0];
+    const ADDRESS_VERSION1_BYTES: &'static [u8; 5] = &[0x03, 0xED, 0x73, 0x45, 0xC0];
+    // prefix version bytes for exporting private key to WIF format
+    // much like bitcoin WIF format addresses will be prefixed with K or L if it corresponds to a compressed public key or 5 if an uncompressed public key
+    const WIF_VERSION1_PREFIX_BYTES: &'static [u8; 1] = &[0x80];
+    // suffix version bytes for signaling that the exported private key in WIF format was used to derive its address from a compressed public key
+    const WIF_VERSION1_COMPRESSED_BYTES: &'static [u8; 1] = &[0x01];
 
-    version_bytes.iter().for_each(|item| {
-        vec_of_ripe_sha_pub_key.push(*item);
-    });
-    
-    // save the resulting version bytes + ripe(sha(pubkey))
-    vec_of_ripe_sha_pub_key.rotate_right(version_bytes.len());
-    println!("Version Bytes + RIPEMD-160 Sha256 Compressed Public Key:");
-    println!("{:X?}", vec_of_ripe_sha_pub_key);
-    println!("");
+    pub fn new() -> Self {
+        let private_key: elliptic_curve::SecretKey<Secp256k1> = SecretKey::random(&mut OsRng);
+        let public_key: elliptic_curve::PublicKey<Secp256k1> = private_key.public_key();
+        let address: String = Wallet::generate_address(&public_key);
+        let signing_key: SigningKey = SigningKey::from(&private_key);
+        let verifying_key: VerifyingKey = VerifyingKey::from(&public_key);
+        let wif_private_key: String = Wallet::generate_wif_private_key(&private_key, true);
+        
+        Self {
+            private_key,
+            public_key,
+            address,
+            signing_key,
+            verifying_key,
+            wif_private_key,
+        }
+    }
 
-    // double sha256 the resulting version bytes + ripe(sha(pub key)) to get checksum 
-    // create a copy of the version bytes + ripe(sha(pub))
-    let copy_ripe_sha_pub = &vec_of_ripe_sha_pub_key[..];
+    // pub fn from(WalletFile) -> Self {
 
-    let mut sha256_hasher: Sha256 = Sha256::new();
-    sha256_hasher.update(copy_ripe_sha_pub);
-    let first_sha256 = sha256_hasher.finalize();
-    println!("First Sha256 Version Bytes + RIPEMD-160 Sha256 Compressed Public Key:");
-    println!("{:X?}", first_sha256);
-    println!("");
+    // }
 
-    let mut sha256_hasher: Sha256 = Sha256::new();
-    sha256_hasher.update(first_sha256);
-    let second_sha256 = sha256_hasher.finalize();
-    println!("Second Sha256 Version Bytes + RIPEMD-160 Sha256 Compressed Public Key:");
-    println!("{:X?}", second_sha256);
-    println!("");
+    pub fn public_key(&mut self) -> PublicKey {
+        self.public_key.clone()
+    }
 
-    // push the first 4 bytes of the second sha as the checksum at the end of the version bytes + ripe(sha(pub))
-    vec_of_ripe_sha_pub_key.push(second_sha256[0]);
-    vec_of_ripe_sha_pub_key.push(second_sha256[1]);
-    vec_of_ripe_sha_pub_key.push(second_sha256[2]);
-    vec_of_ripe_sha_pub_key.push(second_sha256[3]);
-    println!("Version Bytes + RIPEMD-160 Sha256 Compressed Public Key + Checksum:");
-    println!("{:X?}", vec_of_ripe_sha_pub_key);
-    println!("");
+    pub fn address(&mut self) -> String {
+        self.address.clone()
+    }
 
-    // convert to base58 encoded string to get block Addres
-    let encoded = bs58::encode(vec_of_ripe_sha_pub_key).into_string();
-    println!("block Address");
-    println!("{:?}", encoded);
-    println!("");
+    pub fn wif_private_key(&mut self) -> String {
+        self.wif_private_key.clone()
+    }
+
+    // pub fn save_wallet_file() {
+
+    // }
+
+    // pub fn sign_transaction(&t: Transaction) -> Signature {
+    //     Self::sign(t);
+    // }
+
+    // ToDo: what other public functions are needed from a wallet?
+
+    fn sign(&mut self, message: &[u8]) -> Signature{
+        return self.signing_key.sign(message)
+    }
+
+    fn generate_address(public_key: &elliptic_curve::PublicKey<Secp256k1>) -> String {
+        // block addresses are generated in a similar way to version 1 bitcoin addresses
+        // the general process can be found here: https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses#How_to_create_Bitcoin_Address
+
+        // sha256(compressed public key)
+        let mut sha256_hasher: Sha256 = Sha256::new();
+        // convert to [u8] array in comprsesed format with 0x02 or 0x03 prefix (if y is even/odd)
+        sha256_hasher.update(public_key.to_sec1_bytes());
+        let sha256_compressed_pub_key = sha256_hasher.finalize();
+
+        // ripemd160(sha256(compressed public key))
+        let mut ripemd160_hasher: Ripemd160 = Ripemd160::new();
+        ripemd160_hasher.update(sha256_compressed_pub_key);
+        let ripemd_sha256_pub_key = ripemd160_hasher.finalize();
+
+        // version bytes + ripemd160(sha256(compressed public key))
+        let mut vec_of_ripe_sha_pub_key = ripemd_sha256_pub_key.to_vec();
+        // push the version bytes to the end
+        Self::ADDRESS_VERSION1_BYTES.iter().for_each(|item| {
+            vec_of_ripe_sha_pub_key.push(*item);
+        });
+        // then rotate the vector so the version bytes are at the front
+        vec_of_ripe_sha_pub_key.rotate_right(Self::ADDRESS_VERSION1_BYTES.len());
+
+        // sha256(sha256(version bytes + ripemd160(sha256(compressed public key)))) to get checksum 
+        let mut sha256_hasher: Sha256 = Sha256::new();
+        sha256_hasher.update(&vec_of_ripe_sha_pub_key);
+        let first_sha256 = sha256_hasher.finalize();
+
+        let mut sha256_hasher: Sha256 = Sha256::new();
+        sha256_hasher.update(first_sha256);
+        let second_sha256 = sha256_hasher.finalize();
+
+        // version bytes + ripemd160(sha256(compressed public key)) + first 4 bytes of checksum
+        // push the first 4 bytes of the second sha as the checksum at the end of the version bytes + ripe(sha(pub))
+        vec_of_ripe_sha_pub_key.push(second_sha256[0]);
+        vec_of_ripe_sha_pub_key.push(second_sha256[1]);
+        vec_of_ripe_sha_pub_key.push(second_sha256[2]);
+        vec_of_ripe_sha_pub_key.push(second_sha256[3]);
+
+        // base58 encode version bytes + ripemd160(sha256(compressed public key)) + first 4 bytes of checksum
+        bs58::encode(vec_of_ripe_sha_pub_key).into_string()
+    }
+
+    fn generate_wif_private_key(private_key: &elliptic_curve::SecretKey<Secp256k1>, compressed: bool) -> String {
+        // private keys are encoded in WIF format similar to bitcoin's WIF format
+        // the general process can be found here: https://en.bitcoin.it/wiki/Wallet_import_format#Private_key_to_WIF
+
+        // WIF version bytes + private key
+        let mut private_key_vec = private_key.to_bytes().to_vec();
+        // push the version bytes to the front
+        Self::WIF_VERSION1_PREFIX_BYTES.iter().for_each(|item| {
+            private_key_vec.push(*item);
+        });
+        // then rotate the vector so the version bytes are at the front
+        private_key_vec.rotate_right(Self::WIF_VERSION1_PREFIX_BYTES.len());
+
+        // if the private key is used to derive addresses from a compressed public key append the suffix bytes to the end
+        if compressed {
+            Self::WIF_VERSION1_COMPRESSED_BYTES.iter().for_each(|item| {
+                private_key_vec.push(*item);
+            });
+        }
+
+        // sha256(sha256(version bytes + ripemd160(sha256(compressed public key)))) to get checksum 
+        let mut sha256_hasher: Sha256 = Sha256::new();
+        sha256_hasher.update(&private_key_vec);
+        let first_sha256 = sha256_hasher.finalize();
+
+        let mut sha256_hasher: Sha256 = Sha256::new();
+        sha256_hasher.update(first_sha256);
+        let second_sha256 = sha256_hasher.finalize();
+
+        // version bytes + ripemd160(sha256(compressed public key)) + first 4 bytes of checksum
+        // push the first 4 bytes of the second sha as the checksum at the end of the version bytes + ripe(sha(pub))
+        private_key_vec.push(second_sha256[0]);
+        private_key_vec.push(second_sha256[1]);
+        private_key_vec.push(second_sha256[2]);
+        private_key_vec.push(second_sha256[3]);
+
+        // base58 encode version bytes + ripemd160(sha256(compressed public key)) + first 4 bytes of checksum
+        bs58::encode(private_key_vec).into_string()
+    }
 }
